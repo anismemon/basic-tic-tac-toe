39d1dd2e61bc4572f1d559a3dfd4bf1e3fa4f631
anismemon <58182830+anismemon@users.noreply.github.com>
2020-07-29 10:51:37 -0400

    changed public folder to docs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/docs'))
 
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname + '/index.html'));
